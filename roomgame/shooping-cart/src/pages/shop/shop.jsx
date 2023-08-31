bda6aa8ef4e04260aa8ce0f5d122a09746615256
gusti3111 <gusti.mispuansyah@gmail.com>
2023-08-31 08:27:57 +0700

    shopping-cart with react

---
+++ roomgame/shooping-cart/src/pages/shop/shop.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Shop = () => {
+  return(<div className="shop">
+    <div className="Title">
+        <h1>Toko Cabe Online</h1>
+    </div>
+    <div className="products">
+        
+    </div>
+  </div>) ;
+};
